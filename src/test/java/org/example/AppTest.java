8e94e7aac9c7004f5c97f303504c201b4d51b611
fish895623 <dan990429@gmail.com>
2021-08-08 19:52:48 +0900

    gradle build

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,12 +1,12 @@
 package org.example;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class AppTest {
     @Test
     public void shouldAnswerWithTrue() {
-        assertTrue( true );
+        assertTrue(true);
     }
 }

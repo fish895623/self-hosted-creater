811bc7168219a6c171835c6ae946b1fb1c71c3fa
fish895623 <dan990429@gmail.com>
2021-08-08 21:36:07 +0900

    Set up KubeConfing

---
+++ src/main/java/org/example/kubernetes.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+import io.kubernetes.client.openapi.ApiClient;
+import io.kubernetes.client.openapi.ApiException;
+import io.kubernetes.client.openapi.Configuration;
+import io.kubernetes.client.openapi.apis.CoreV1Api;
+import io.kubernetes.client.openapi.models.V1Pod;
+import io.kubernetes.client.openapi.models.V1PodList;
+import io.kubernetes.client.util.Config;
+import io.kubernetes.client.util.KubeConfig;
+
+public class kubernetes {
+    public static void main(String[] args) throws IOException, ApiException {
+        ApiClient client = Config.defaultClient();
+        // TODO Use Writer to
+        KubeConfig.loadKubeConfig(new FileReader("/home/dan99/.kube/config"));
+        Configuration.setDefaultApiClient(client);
+        CoreV1Api api = new CoreV1Api();
+
+        V1PodList list = api.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null, null);
+        for (V1Pod item : list.getItems()) {
+            System.out.println(item.getMetadata().getName());
+        }
+    }
+}

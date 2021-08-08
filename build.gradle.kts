bdbdda2c7697ac53d664e9deb417d2909ef7634a
fish895623 <dan990429@gmail.com>
2021-08-09 02:47:00 +0900

    Change to gradle kotlin dsl

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+@file:Suppress("SpellCheckingInspection")
+
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    java
+    `maven-publish`
+    application
+    id("com.github.johnrengelman.shadow") version "7.0.0"
+}
+
+repositories {
+    mavenLocal()
+    maven {
+        url = uri("https://repo.maven.apache.org/maven2/")
+    }
+}
+
+dependencies {
+    implementation("com.google.code.gson:gson:2.8.7")
+    testImplementation("junit:junit:4.13.2")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+description = "untitled3"
+
+application {
+    mainClass.set("org.example.App")
+}
+
+tasks {
+    named<ShadowJar>("shadowJar") {
+        mergeServiceFiles()
+        manifest {
+            attributes(mapOf("Main-Class" to "org.example.App"))
+        }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}

b6fef10fd29d353e385a530e0100a7472d7878a3
fish895623 <dan990429@gmailc.om>
2021-08-15 00:43:02 +0900

    liveoutput

--- src/kube.py
+++ src/kube.py
@@ -9,8 +9,13 @@ def kubectl_version() -> str:
 
 
 def run(args: str):
+    with Popen(args.split(), stdout=PIPE, stderr=PIPE) as process:
+        return process.communicate()
+
+
+def run_liveoutput(args: str):
     with Popen(args.split(), stdout=PIPE, stderr=PIPE, shell=True) as process:
-        with open('test.log', 'w') as f:
-            for c in iter(lambda: process.stdout.read(1), ''):
-                sys.stdout.write(c.decode('utf-8'))
-                f.write(c.decode('utf-8'))
+        with open("test.log", "w") as f:
+            for c in iter(lambda: process.stdout.read(1), ""):
+                sys.stdout.write(c.decode("utf-8"))
+                f.write(c.decode("utf-8"))
